8ec5684b683daa63ea35ed4ea3b71d1fa1081575
Tom Kotopoulos <kotopoulos10@gmail.com>
2019-01-28 18:26:27 -0500

    In class assignment complete

--- DungeonDice/ViewController.swift
+++ DungeonDice/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var rolledMessage: UILabel!
+    var roll: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func rollPressed(_ sender: UIButton) {
+        let rollUpTo = sender.tag
+        roll = Int.random(in: 1...rollUpTo)
+        rolledMessage.text = "You rolled a \(rollUpTo) sided dice and got a \(roll)"
+    }
+    
 }
 
